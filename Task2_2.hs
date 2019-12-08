43ae557b452f1c3ee491dcf1afa7b24aa638f841
stanislav <iluvstrwrs@mail.ru>
2019-12-08 19:39:10 +0300

    revert 2_2

--- Task2_2.hs
+++ Task2_2.hs
@@ -12,17 +12,13 @@ import Prelude hiding (foldl, foldr, unfoldr, map, concatMap,
     filter, maxBy, minBy, reverse, sum, product, elem)
 
 foldl :: (b -> a -> b) -> b -> [a] -> b
-foldl f z [] = z
-foldl f z (h:t) = foldl f (f z h) t
+foldl = todo
 
 foldr :: (a -> b -> b) -> b -> [a] -> b
-foldr f z [] = z
-foldr f z (h:t) = f h (foldr f z t)
+foldr = todo
 
 unfoldr :: (b -> Maybe (a, b)) -> b -> [a]
-unfoldr f a = case f a of
-               Just (h, next) -> h:(unfoldr f next)
-               Nothing      -> []
+unfoldr = todo
 
 -- Сумма всех элементов списка (пример)
 sum :: [Integer] -> Integer
@@ -34,49 +30,37 @@ reverse lst = foldl f [] lst where f t h = h:t
 
 -- Отображение элементов списка
 map :: (a -> b) -> [a] -> [b]
-map f lst = foldr (mapFunc f) [] lst
+map = todo
 
-mapFunc :: (a -> b) -> a -> [b]-> [b]
-mapFunc f h t =  ((f h) : t)
 -- Произведение всех элементов списка
 product :: [Integer] -> Integer
-product = foldl (*) 1
+product = todo
 
 -- Выделение из списка Maybe всех существующих значений
 catMaybes :: [Maybe a] -> [a]
-catMaybes = foldr (\i lst -> case i of
-                                 Just i -> i:lst
-                                 Nothing -> lst) []
+catMaybes = todo
 
 -- Диагональ матрицы
 diagonal :: [[a]] -> [a]
-diagonal [] = []
-diagonal mat = if  length (head mat) == length (mat) then diagonal' mat 0
-else []
-
-diagonal' :: [[a]] -> Int -> [a]
-diagonal' mat n = if length(mat)== 0 then [] else let h = (head (mat) !! n)
-                  in  h:(diagonal' (drop 1 mat) (n+1))
+diagonal = todo
 
 -- Фильтр для всех элементов, не соответствующих предикату
 filterNot :: (a -> Bool) -> [a] -> [a]
-filterNot p = foldr (\x s -> if p x then s else x:s) []
+filterNot = todo
 
 -- Поиск элемента в списке
 elem :: (Eq a) => a -> [a] -> Bool
-elem e [] = False
-elem e lst = foldl (||) False (map (\x -> x == e) lst)
+elem = todo
 
 -- Список чисел в диапазоне [from, to) с шагом step
 rangeTo :: Integer -> Integer -> Integer -> [Integer]
-rangeTo from to step = unfoldr (\x -> if x < to then Just (x, x+step) else Nothing) from
-
+rangeTo from to step = todo
 
 -- Конкатенация двух списков
 append :: [a] -> [a] -> [a]
-append first second = foldr (\x res -> x:res)  second first
+append = todo
 
 -- Разбиение списка lst на куски размером n
 -- (последний кусок может быть меньше)
 groups :: [a] -> Integer -> [[a]]
-groups lst n = unfoldr (\x -> if null x then Nothing else Just (take (fromIntegral n) x, drop (fromIntegral n) x) ) lst
+groups lst n = todo
