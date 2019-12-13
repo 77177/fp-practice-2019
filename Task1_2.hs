a8b645040cda32a15a51f0b22e9e83b25edffe65
Stanislav_Tarasov <iluvstrwrs@mail.ru>
2019-12-13 12:47:58 +0300

    added 2 impl

--- Task1_2.hs
+++ Task1_2.hs
@@ -13,11 +13,17 @@ sin x = todo
 
 -- косинус числа (формула Тейлора)
 cos :: Double -> Double
-cos x = todo
+fac :: Int -> Int
+fac n = if (n == 0) then 1 else n * fac (n-1)
+
+cos x = sum [cos | k <- [0..],
+                  let cos = (-1) * (x^(2*k) / fromIntegral (fac (2*k))) ,
+                  abs (cos) > 0.001]
 
 -- наибольший общий делитель двух чисел
 gcd :: Integer -> Integer -> Integer
-gcd x y = todo
+
+gcd 0 y = todo
 
 -- существует ли полный целочисленный квадрат в диапазоне [from, to)?
 doesSquareBetweenExist :: Integer -> Integer -> Bool
@@ -31,11 +37,15 @@ isDateCorrect day month year = todo
 -- возведение числа в степень, duh
 -- готовые функции и плавающую арифметику использовать нельзя
 pow :: Integer -> Integer -> Integer
-pow x y = todo
+pow x y = if y /= 1 then x * pow (x) (y-1) else x
 
 -- является ли данное число простым?
 isPrime :: Integer -> Bool
-isPrime x = todo
+
+isPrime 1 = False
+isPrime 2 = True
+isPrime x = if (length [y | y <- [2 .. x-1], mod x y == 0]) > 0 then False else True
+
 
 type Point2D = (Double, Double)
 
