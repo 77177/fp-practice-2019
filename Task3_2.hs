63678a49ef531813b79117917b6ad558bbc7cbe6
stanislav <iluvstrwrs@mail.ru>
2020-01-03 18:30:33 +0300

    added 2_1

--- Task3_2.hs
+++ Task3_2.hs
@@ -13,9 +13,35 @@ import Todo(todo)
 data ReverseList a = RNil | RCons (ReverseList a) a
 
 rlistToList :: ReverseList a -> [a]
-rlistToList = todo
+rlistToList revlist = case revlist of
+                        RNil -> []
+                        (RCons rlst element) -> concat [rlistToList rlst, [element]]
 
 listToRList :: [a] -> ReverseList a
-listToRList = todo
+listToRList list  = case list of
+                        [] -> RNil
+                        list -> RCons (listToRList (init list)) (last list)
 
 -- Реализуйте классы Eq, Ord, Show, Monoid, Functor
+
+instance (Show a) => Show (ReverseList a) where
+    show revlist = case revlist of
+                        RNil -> ""
+                        (RCons RNil element) -> show element
+                        (RCons rlist element) -> show element ++ " " ++ show rlist
+
+instance (Eq a) => Eq (ReverseList a) where
+
+    (==) leftList rightList = (rlistToList leftList) == (rlistToList rightList)
+
+instance (Ord a) => Ord (ReverseList a) where
+    compare leftList rightList = compare (rlistToList leftList) (rlistToList rightList)
+
+instance Monoid (ReverseList a) where
+
+    mempty = RNil
+    mappend leftList rightList = listToRList (mappend (rlistToList leftList) (rlistToList rightList))
+
+instance Functor ReverseList where
+
+    fmap f rlist = listToRList (fmap f (rlistToList rlist))
