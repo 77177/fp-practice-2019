84afb89b428402fe66df5d1b557618efe07c3e4d
stanislav <iluvstrwrs@mail.ru>
2020-01-04 16:17:10 +0300

    monad not finished

--- Task4_2.hs
+++ Task4_2.hs
@@ -11,3 +11,47 @@ data FourOf a = FourOf a a a a deriving(Show,Eq)
 -- реализуйте классы `Functor`, `Applicative` и `Monad` для типа `FourOf`
 -- таким образом, что
 -- do { x <- FourOf 1 2 3 4; y <- FourOf 4 6 7 8; return $ x + y } === FourOf 5 8 10 12
+
+
+
+-- функтор имеет стандартное поведение - просто берем все значения контейнера и применяем к ним функцию
+-- функтор должен попадать под ограничения
+
+-- проход по функтору с помощью функции тождественного отображения возвращает неизменный функтор
+--      fmap id = id
+
+-- Если 2 последовательные операции обхода выподнятюся друг за другом,
+-- то результат должен быть идентичен проходу 1-й композитной функции, которая состоит из этих двух
+--      fmap (f . g)  ==  fmap f . fmap g
+
+
+instance Functor FourOf where
+    fmap func (FourOf first second third fourth) = (FourOf (func first) (func second) (func third) (func fourth))
+
+-- аппликативный функтор имеет схожее поведение. На этот раз функции, которые сами находятся в контейнерах применяются по одной
+-- дл каждого аргумента из функтора
+
+-- аппликативный функтор так же попадает под ограничения
+
+-- pure id <*> v = v
+-- pure f <*> pure x = pure (f x)
+-- u <*> pure y = pure ($ y) <*> u
+-- pure (.) <*> u <*> v <*> w = u <*> (v <*> w)
+
+
+instance Applicative FourOf where
+    pure a = FourOf a a a a
+    (<*>) (FourOf frst sec thrd frth) (FourOf a b c d) = (FourOf (frst a) (sec b) (thrd c) (frth d))
+
+
+-- (>>=) :: m a -> (a -> m b) -> m b
+
+-- (>>=) :: m (a a a a) -> (a -> m b) -> m b
+
+--пока не работает
+
+instance Monad FourOf where
+    return = pure
+    (>>=) (FourOf frst sec thrd frth) func = FourOf (frst) (sec) (thrd) (frth)
+
+
