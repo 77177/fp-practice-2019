33009d42107234ea243020f23d3f47a3ea62a2c6
stanislav <iluvstrwrs@mail.ru>
2020-01-04 19:07:27 +0300

    fixed monad 4_2

--- Task4_2.hs
+++ Task4_2.hs
@@ -6,7 +6,7 @@ module Task4_2 where
   Реализуйте все требуемые классы типов и объясните, почему они реализованы именно так.
 -}
 
-data FourOf a = FourOf a a a a deriving(Show,Eq)
+data FourOf a = FourOf {one::a,two::a,three::a,four::a} deriving(Show,Eq)
 
 -- реализуйте классы `Functor`, `Applicative` и `Monad` для типа `FourOf`
 -- таким образом, что
@@ -15,7 +15,7 @@ data FourOf a = FourOf a a a a deriving(Show,Eq)
 
 
 -- функтор имеет стандартное поведение - просто берем все значения контейнера и применяем к ним функцию
--- функтор должен попадать под ограничения
+-- функтор должен попадать под ограничения:
 
 -- проход по функтору с помощью функции тождественного отображения возвращает неизменный функтор
 --      fmap id = id
@@ -29,9 +29,12 @@ instance Functor FourOf where
     fmap func (FourOf first second third fourth) = (FourOf (func first) (func second) (func third) (func fourth))
 
 -- аппликативный функтор имеет схожее поведение. На этот раз функции, которые сами находятся в контейнерах применяются по одной
--- дл каждого аргумента из функтора
+-- для каждого аргумента из функтора
 
 -- аппликативный функтор так же попадает под ограничения
+
+-- функция pure просто берет элемен и заполняет им все места в FourOf
+
 
 -- pure id <*> v = v
 -- pure f <*> pure x = pure (f x)
@@ -44,14 +47,16 @@ instance Applicative FourOf where
     (<*>) (FourOf frst sec thrd frth) (FourOf a b c d) = (FourOf (frst a) (sec b) (thrd c) (frth d))
 
 
+-- Для имплементации функциональности монады нужно реализовать функцию bind и return.
+-- bind будет брать экземпляр FourOf и для каждого и 4-х значений делать монаду, из которой потом нужно взять соответствующее позиции
+-- значения. После этого все 4 значения снова обварачиваются в монаду.
+
 -- (>>=) :: m a -> (a -> m b) -> m b
 
--- (>>=) :: m (a a a a) -> (a -> m b) -> m b
-
---пока не работает
+-- при данной имплементации все ограничения монад ((return x >>= f) = f x, ассоциативность bind функции и (x >>= (\a -> return a)) = x) соблюдены
 
 instance Monad FourOf where
     return = pure
-    (>>=) (FourOf frst sec thrd frth) func = FourOf (frst) (sec) (thrd) (frth)
+    (>>=) (FourOf frst sec thrd frth) func = FourOf (one (func frst)) (two (func sec)) (three (func thrd)) (four (func frth))
 
-
+-- do { x <- FourOf 1 2 3 4; y <- FourOf 4 6 7 8; return $ x + y } === FourOf {one = 5, two = 8, three = 10, four = 12}
